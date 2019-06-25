b9f364d71130d85a3e343356dfd81c767b7d3f79
FirassMraihi <firassmraihi0@gmail.com>
2019-06-25 12:35:12 +0200

    fixing login form size

--- Front Application/frontappnews/src/components/FilterNews.vue
+++ Front Application/frontappnews/src/components/FilterNews.vue
@@ -1,9 +1,14 @@
 <template>
-<div  class="filediv">
-    <p>Please Choose A configuration File </p>
-    <p>If you don't have the structure please Download the file then modify the content then
+<div  class="fileform">
+    <br><br>
+    <h1 class="ConfigTitle">News  Configuration </h1>
+    <hr class="seperatorlogin">
+    <p class="text">If you don't have the structure please Download the file then modify the content then
         upload it again</p>
-    <input type="file" value="GET FILE" />
+    <input type="file" value="GET FILE" class="bt" />
+    <br>
+    <button class="bt">
+         <label class="Log-Intxt">Download</label></button>
 </div>
 </template>
 <style>
